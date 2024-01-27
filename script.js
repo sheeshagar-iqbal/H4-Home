1c9cf28e0156eb3b5ddfbb3b693bda97bcc4a701
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2024-01-27 13:12:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// var b = +prompt("enterthe value of b");
+// var c  = +prompt("enterthe value of c");
+// function iop(a,b) {
+//     return a+b;
+// }
+// var s=iop();
+// var s = a + b;
+// console.log(`the sum ${a} and ${b} is ${s}`);
+
+// console.log(`hello ${a},your age is ${b} old`);
+
+//  function fun{
+
+// if(a>b){
+//     console.log("a great");}
+//   else if(b>c)  {
+//     console.log("b great");} 
+    
+//         else{
+//             console.log("c");
+
+//         } else if(b===c)  {
+//     console.log("b nd c are same");} }
+//        fun();
+var a  = prompt("enterthe value of user ");
+        if(a==="m" || a==="M"){
+               console.log("good morning mem ");
+        }
+        else if(a==="s" || a==="S")
+            console.log("good morning  sir ");
+     else{
+        console.log("not");
+     }
